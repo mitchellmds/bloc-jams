85099286570580f28ffc25b29eaeceb8101732eb
Steve <mitchellmds@gmail.com>
2017-05-18 06:41:25 -0700

    assignment 23 DOM checkpoint assignment complete

---
+++ scripts/utilities.js
@@ -0,0 +1,12 @@
+var points = [
+  "points.style.opacity = 1;",
+  "points.style.transform = \"scaleX(1) translateY(0)",
+  "points.style.msTransform = \"scaleX(1) translateY(0)",
+  "points.style.WebkitTransform = \"scaleX(1) translateY(0)"
+]
+var forEach = function(points) {
+  console.log(points);
+
+};
+
+points.forEach(forEach);
