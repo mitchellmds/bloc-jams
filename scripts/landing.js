ba8c711f1a244ed3d9ad27069edcb02097c258eb
Steve <mitchellmds@gmail.com>
2017-05-18 06:34:17 -0700

    assignment 23 DOM checkpoint assignment complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,39 +2,20 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(point) {
 
+  var points = [
+    "points.style.opacity = 1;",
+    "points.style.transform = \"scaleX(1) translateY(0)",
+    "points.style.msTransform = \"scaleX(1) translateY(0)",
+    "points.style.WebkitTransform = \"scaleX(1) translateY(0)"
+  ]
+  var forEach = function(points) {
+    console.log(points);
+
+  };
+
+  points.forEach(forEach);
 
 
-      var revealFirstPoint = function() {
-          points[0].style.opacity = 1;
-          points[0].style.transform = "scaleX(1) translateY(0)";
-          points[0].style.msTransform = "scaleX(1) translateY(0)";
-          points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-      };
-
-      var revealPoint = function(index) {
-          points[index].style.opacity = 1;
-          points[index].style.transform = "scaleX(1) translateY(0)";
-          points[index].style.msTransform = "scaleX(1) translateY(0)";
-          points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-      };
-
-      var revealSecondPoint = function() {
-          points[1].style.opacity = 1;
-          points[1].style.transform = "scaleX(1) translateY(0)";
-          points[1].style.msTransform = "scaleX(1) translateY(0)";
-          points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-      };
-
-      var revealThirdPoint = function() {
-          points[2].style.opacity = 1;
-          points[2].style.transform = "scaleX(1) translateY(0)";
-          points[2].style.msTransform = "scaleX(1) translateY(0)";
-          points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-      };
-
-      revealFirstPoint();
-      revealSecondPoint();
-      revealThirdPoint();
 
   };
 
@@ -42,7 +23,7 @@ var animatePoints = function(point) {
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
-    
+
      var sellingPoints = document.getElementsByClassName('selling-points')[0];
 
      window.addEventListener('scroll', function(event) {
