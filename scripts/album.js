6f676cbb8086d4e38365f28f951dabc6d21c3ba4
Steve <mitchellmds@gmail.com>
2017-05-23 07:32:10 -0700

    checkpoint 27 completed

--- scripts/album.js
+++ scripts/album.js
@@ -85,9 +85,19 @@ var setCurrentAlbum = function(album) {
         var currentParent = element.parentElement;
         while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
+            if (currentParent) {
+ while (currentParent.className && currentParent.className != targetClass) {
+    currentParent = currentParent.parentElement;
+         }
+
+         if (currentParent.className == targetClass) {
+             return currentParent;
+         } else {
+             alert("No parent with that class name found.");
+         }
+     } else {
+         alert("No parent found.");
         }
-        return currentParent;
-    }
 };
 
 var getSongItem = function(element) {
