523b6e3bae17b4be272841a7f046f0d2a4b6564e
Steve <mitchellmds@gmail.com>
2017-06-03 08:58:19 -0700

    assignment 33 completed

--- scripts/album.js
+++ scripts/album.js
@@ -179,6 +179,19 @@ var updatePlayerBarSong = function() {
      $lastSongNumberCell.html(lastSongNumber);
  };
 
+ var togglePlayFromPlayerbar = function() {
+     var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+    if (currentSoundFile.isPaused()) {
+        $currentlyPlayingCell.html(pauseButtonTemplate);
+        $(this).html(playerBarPauseButton);
+        currentSoundFile.play();
+    } else if (currentSoundFile) {
+        $currentlyPlayingCell.html(playButtonTemplate);
+        $(this).html(playerBarPlayButton);
+        currentSoundFile.pause();
+    }
+};
+
  var previousSong = function() {
     var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
     currentSongIndex--;
@@ -215,12 +228,14 @@ var playerBarPlayButton = '<span class="ion-play"></span>';
  var currentVolume = 80;
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
+ var $playPauseButton = $('.main-controls .play-pause');
 
 
  $(document).ready(function() {
      setCurrentAlbum(albumPicasso);
      $previousButton.click(previousSong);
      $nextButton.click(nextSong);
+     $playPauseButton.click(togglePlayFromPlayerbar);
  });
 
  var albums = [albumPicasso, albumMarconi, albumGodOfLamb];
